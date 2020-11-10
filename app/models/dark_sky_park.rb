2a427b9ab3ac6a4e920eb3153bd4fe108c248936
jpeterson14 <jpeterson0514@gmail.com>
2020-11-10 10:13:07 -0600

    updated assocations

--- app/models/dark_sky_park.rb
+++ app/models/dark_sky_park.rb
@@ -4,8 +4,8 @@ class DarkSkyPark < ApplicationRecord
     has_many :bucketlist_locations
     has_many :visitors, through: :bucketlist_locations
     has_many :reviews
-    has_many :visitors, through: :reviews
+    has_many :rev_visitors, through: :reviews, source: :visitor
     has_many :comments
-    has_many :visitors, through: :comments
+    has_many :com_visitors, through: :comments, source: :visitor
 
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,7 +2,7 @@ class Visitor < ApplicationRecord
     has_many :bucketlist_locations, :dependent => :destroy
     has_many :dark_sky_parks, through: :bucketlist_locations
     has_many :reviews, :dependent => :destroy
-    has_many :dark_sky_parks, through: :reviews
+    has_many :rev_parks, through: :reviews, source: :dark_sky_park
     has_many :comments, :dependent => :destroy
-    has_many :dark_sky_parks, through: :comments
+    has_many :com_parks, through: :comments, source: :dark_sky_park
 end
